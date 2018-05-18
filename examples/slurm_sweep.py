57089f365059915df060dcfaba9e8e2944eff675
wangbx66 <wangbx66@gmail.com>
2018-05-18 13:16:18 +0800

    mutex

--- examples/slurm_sweep.py
+++ examples/slurm_sweep.py
@@ -20,6 +20,10 @@ def hold_mutex():
     with open(os.path.expanduser('~/.mujoco/mutex'), 'w') as fw:
         fw.write('occupied')
 
+def release_mutex():
+    with open(os.path.expanduser('~/.mujoco/mutex'), 'w') as fw:
+        fw.write('available')
+
 
 command_prefix =\
 """#!/bin/bash
@@ -40,14 +44,18 @@ import itertools
 configs = itertools.product(*config_list)
 
 index = 0
-for config in configs:
-    file_name = 'job_'+str(index)+'.sh'
-    command = 'python rb_ppo_gym.py --env-name {env} --seed {seed} --learning-rate {lr} --max-iter-num {max_iter} --logger-name {env}-k{k}s{seed}-{method} --number-subspace {k}'.format(env=config[0], seed=config[1], k=config[2], method=config[3], lr='3e-3' if config[0].startswith('Quadratic') else '3e-4', max_iter=max_iter)
-    file_string = command_prefix+command+'\n'
-    with open(file_name, 'w') as f:
-        f.write(file_string)
-    index += 1
-    wait_mutex()
-    hold_mutex()
-    os.system("sbatch {}".format(file_name))
-    os.remove(file_name)
+try:
+    for config in configs:
+        file_name = 'job_'+str(index)+'.sh'
+        command = 'python rb_ppo_gym.py --env-name {env} --seed {seed} --learning-rate {lr} --max-iter-num {max_iter} --logger-name {env}-k{k}s{seed}-{method} --number-subspace {k}'.format(env=config[0], seed=config[1], k=config[2], method=config[3], lr='3e-3' if config[0].startswith('Quadratic') else '3e-4', max_iter=max_iter)
+        file_string = command_prefix+command+'\n'
+        with open(file_name, 'w') as f:
+            f.write(file_string)
+        index += 1
+        wait_mutex()
+        hold_mutex()
+        os.system("sbatch {}".format(file_name))
+        os.remove(file_name)
+except KeyboardInterrupt:
+    release_mutex()
+    
